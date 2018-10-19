0876627b6a1355d04d54495a7daffb72503df063
Production611 <Production6115338@gmail.com>
2018-10-19 10:58:31 +0700

    TurnON Bluetooth Success

--- app/src/main/java/com/example/ruchadapong/projectapp/ServiceFragment.java
+++ app/src/main/java/com/example/ruchadapong/projectapp/ServiceFragment.java
@@ -1,5 +1,8 @@
 package com.example.ruchadapong.projectapp;
 
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,22 +11,57 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ServiceFragment extends Fragment {
 
+    private static final int REQUEST_ENABLE_BT = 0;
+
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        
-        selectBluetooth();
+
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+
+            Toast.makeText(getActivity(),"Device Not Supported",Toast.LENGTH_LONG).show();
+            getActivity().finish();
+        }
+
+        final Button button = getView().findViewById(R.id.btnOn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!bluetoothAdapter.isEnabled()){
+
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent,REQUEST_ENABLE_BT);
+
+                }
+            }
+        });
+
+        final Button button1 = getView().findViewById(R.id.btnOff);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bluetoothAdapter.disable();
+                Toast.makeText(getActivity(),"TURN OFF",Toast.LENGTH_LONG).show();
+                getActivity().finish();
+            }
+        });
+
+
+
+
+
 
         createToolbar();
-    }
-
-    private void selectBluetooth() {
     }
 
 
