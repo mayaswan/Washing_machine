3999d09ad4c6a8008a375818a5e1292ba589ef0c
mayaswan <33310757+mayaswan@users.noreply.github.com>
2018-11-29 16:18:55 +0700

    Update ServiceFragment.java

--- app/src/main/java/com/example/project/ServiceFragment.java
+++ app/src/main/java/com/example/project/ServiceFragment.java
@@ -1,499 +1,58 @@
 package com.example.project;
-
-
-
-
-
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class ServiceFragment extends Fragment {
 
-    private final String DEVICE_ADDRESS = "98:D3:36:81:0B:99"; //MAC Address of Bluetooth Module
-    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-
-    private BluetoothDevice device;
-    private BluetoothSocket socket;
-    private OutputStream outputStream;
-    String command;
-    Button btnConnect,numOne,numTwo,numThree,numFour,numFive,numSix,numSeven,numEigth,numNine;
-
-
+    EditText editIp;
+    Button btnOn, btnOff;
+    TextView textInfo1, textInfo2;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        btnConnect = getView().findViewById(R.id.btnConnect);
-        numOne = getView().findViewById(R.id.numOne);
-        numTwo = getView().findViewById(R.id.numTwo);
-        numThree = getView().findViewById(R.id.numThree);
-        numFour = getView().findViewById(R.id.numFour);
-        numFive = getView().findViewById(R.id.numFive);
-        numSix = getView().findViewById(R.id.numSix);
-        numSeven = getView().findViewById(R.id.numSeven);
-        numEigth = getView().findViewById(R.id.numEigth);
-        numNine = getView().findViewById(R.id.numNine);
 
-        numOne.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "F";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-
-                    onStop();
-
-                }
-
-
-                return false;
-            }
-        });
-
-        numTwo.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "B";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-
-        numThree.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "3";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-        numFour.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "4";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-        numFive.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "5";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-
-        numSix.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "6";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-
-        numSeven.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "7";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-
-        numEigth.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "8";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-        numNine.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
-                {
-                    command = "9";
-
-                    try
-                    {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
-                    command = "10";
-                    try
-                    {
-                        outputStream.write(command.getBytes());
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-                return false;
-            }
-        });
-
-
-        btnConnect.setOnClickListener(new View.OnClickListener() {
+        btnOn = getView().findViewById(R.id.bon);
+        btnOff = getView().findViewById(R.id.boff);
+        btnOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (BTinit()) {
-                    BTconnect();
-                }
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference buttonOn = database.getReference("BTN_STATUS");
+                buttonOn.setValue(1);
             }
         });
-
-
-
-
-
-
-        createToolbar();
-    }
-
-    private boolean BTconnect() {
-
-        boolean connected = true;
-
-        try {
-
-            socket = device.createRfcommSocketToServiceRecord(PORT_UUID); //Creates a socket to handle the outgoing connection
-            socket.connect();
-
-            Toast.makeText(getActivity(), "Connection to bluetooth device successful", Toast.LENGTH_SHORT).show();
-
-        }catch (IOException e){
-
-            e.printStackTrace();
-            connected = false;
-
-        }
-
-        if(connected)
-        {
-            try
-            {
-                outputStream = socket.getOutputStream(); //gets the output stream of the socket
-            }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        return connected;
-
+       btnOff.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               FirebaseDatabase database = FirebaseDatabase.getInstance();
+               DatabaseReference buttonOff = database.getReference("BTN_STATUS");
+               buttonOff.setValue(0);
+           }
+       });
 
     }
 
-    private boolean BTinit() {
 
-        boolean found = false;
-
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if(bluetoothAdapter == null) //Checks if the device supports bluetooth
-        {
-            Toast.makeText(getActivity(), "Device doesn't support bluetooth", Toast.LENGTH_SHORT).show();
-        }
-
-        if(!bluetoothAdapter.isEnabled()) //Checks if bluetooth is enabled. If not, the program will ask permission from the user to enable it
-        {
-            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableAdapter, 0);
-
-            try {
-                Thread.sleep(1000);
-
-            }catch (InterruptedException e){
-
-                e.printStackTrace();
-            }
-
-
-        }
-
-        Set<BluetoothDevice> bondedDevice = bluetoothAdapter.getBondedDevices();
-
-        if (bondedDevice.isEmpty()) {
-
-            Toast.makeText(getActivity(), "Please pair the device first", Toast.LENGTH_SHORT).show();
-        }else {
-            for (BluetoothDevice iterator : bondedDevice){
-
-                if (iterator.getAddress().equals(DEVICE_ADDRESS)){
-                    device = iterator;
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-        return found;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    private void createToolbar() {
-
-        Toolbar toolbar = getView().findViewById(R.id.toolbarService);
-        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
-
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Bluetooth Controller");
-
-
-        setHasOptionsMenu(true);
-
-    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_service,container,false);
+
         return view;
     }
-
-
 }
-
-
